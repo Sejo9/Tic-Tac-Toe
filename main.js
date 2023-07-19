9d14a6b2d343baefa06b7b3b4024c805f4371013
Jose Michael Moses <132714659+SejoIX@users.noreply.github.com>
2023-07-19 07:19:57 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/* Global Interactions */
+let p1 = '';
+
+let p2 = '';
+
+let turn = 'X';
+
+const score = {
+    p1: 0,
+    ties: 0,
+    p2: 0
+}
+
+let p2IsCPU = false;
+
+
+
+/* Menu Interactions */
+
+//choice x button
+
+//choice o button
+
+//new game vs cpu button
+
+//new game vs player button
+
+
+//Function to change selected choice
+
+
+/* Game Interactions */
